60942abad16a434822a20f9ac4dd609b86878b28
Sven Thiel <m6thsv2@googlemail.com>
2023-01-13 13:38:42 +0100

    add doi entity to log all issues dois internally

---
+++ Vaelastrasz.Server/Entities/DOI.cs
@@ -0,0 +1,27 @@
+﻿using LiteDB;
+
+namespace Vaelastrasz.Server.Entities
+{
+    public class DOI : BaseEntity
+    {
+        public DOIType Type { get; set; }
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+        public Status Status { get; set; }
+
+        [BsonRef("users")]
+        public User User { get; set; }
+    }
+
+    public enum DOIType
+    {
+        DataCite = 0
+    }
+
+    public enum Status
+    {
+        Draft = 0,
+        Registered = 1,
+        Findable = 2
+    }
+}
