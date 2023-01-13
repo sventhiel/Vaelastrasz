dcc4d2b131c29d994a76d0777b22e88a36e20c1e
Sven Thiel <m6thsv2@googlemail.com>
2023-01-13 13:30:21 +0100

    add abstract common class for all entities

---
+++ Vaelastrasz.Server/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Vaelastrasz.Server.Entities
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; set; }
+        public DateTimeOffset CreationDate { get; set; }
+        public DateTimeOffset LastUpdateDate { get; set; }
+    }
+}
