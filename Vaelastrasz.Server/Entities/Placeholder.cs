d80e5e0cc4317fa3896e249a9b4f7340c435d3bf
Sven Thiel <m6thsv2@googlemail.com>
2023-01-13 13:37:00 +0100

    add placeholder entity for dynamic replacements within the doi

---
+++ Vaelastrasz.Server/Entities/Placeholder.cs
@@ -0,0 +1,13 @@
+﻿using LiteDB;
+
+namespace Vaelastrasz.Server.Entities
+{
+    public class Placeholder : BaseEntity
+    {
+        public string Expression { get; set; }
+        public string RegularExpression { get; set; }
+
+        [BsonRef("users")]
+        public User User { get; set; }
+    }
+}
