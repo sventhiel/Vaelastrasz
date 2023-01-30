d6b45b1b161d3ba25b13e77e4b7d28a79d9c42c9
Sven Thiel <m6thsv2@googlemail.com>
2023-01-30 10:02:39 +0100

    add test project for library project

---
+++ Vaelastrasz.Library.Tests/Models/DataCiteModelTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vaelastrasz.Library.Models;
+using Vaelastrasz.Library.Extensions;
+
+namespace Vaelastrasz.Library.Tests.Models
+{
+    public class DataCiteModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            List<ValidationResult> results = new List<ValidationResult>();
+            CreateDataCiteModel x = new CreateDataCiteModel();
+
+            var result = x.Validate(out results);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
