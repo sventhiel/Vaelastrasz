c704f2204ef036a71b21aa181129f84140614733
Sven Thiel <m6thsv2@googlemail.com>
2023-01-30 10:03:02 +0100

    remove unnecessary base entity

--- Vaelastrasz.Server/Entities/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vaelastrasz.Server.Entities
-{
-    public abstract class BaseEntity
-    {
-        public long Id { get; set; }
-        public DateTimeOffset CreationDate { get; set; }
-        public DateTimeOffset LastUpdateDate { get; set; }
-    }
-}
