52b270f2efbcb2b49b2f030876650f8e74a02c80
Sven Thiel <m6thsv2@googlemail.com>
2023-03-06 12:40:27 +0100

    remove unnecessary config

--- Vaelastrasz.Library/Configuration/DataCiteConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vaelastrasz.Library.Configuration
-{
-    public class DataCiteConfiguration
-    {
-        public string Host { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
