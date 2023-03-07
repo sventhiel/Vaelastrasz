66401a61b2ffeecf6a34186bf68dc7acf6943634
Sven Thiel <m6thsv2@googlemail.com>
2023-03-07 10:21:57 +0100

    update controllers to retrieve jwt

--- Vaelastrasz.Server/Controllers/TokensController.cs
+++ Vaelastrasz.Server/Controllers/TokensController.cs
@@ -1,37 +1,39 @@
 ﻿using LiteDB;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Vaelastrasz.Server.Configuration;
-using Vaelastrasz.Server.Entities;
-using Vaelastrasz.Server.Models;
+using Vaelastrasz.Server.Services;
 
 namespace Vaelastrasz.Server.Controllers
 {
     [Route("api")]
     [ApiController]
-    public class AuthController : ControllerBase
+    [Authorize]
+    public class TokensController : ControllerBase
     {
         private ConnectionString _connectionString;
         private JwtConfiguration _jwtConfiguration;
-        private List<Admin> _admins;
+        private List<string> _admins;
 
-        public AuthController(IConfiguration configuration, ConnectionString connectionString)
+        public TokensController(IConfiguration configuration, ConnectionString connectionString)
         {
             _connectionString = connectionString;
             _jwtConfiguration = configuration.GetSection("JWT").Get<JwtConfiguration>();
-            _admins = configuration.GetSection("Admins").Get<List<Admin>>();
+            _admins = configuration.GetSection("Admins").Get<List<string>>();
         }
 
-        [HttpPost("login")]
-        public IActionResult Login(LoginUserModel model)
+        [HttpGet("token")]
+        public IActionResult Get()
         {
-            //if (User?.Identity?.Name == null)
-            //    return StatusCode(400);
+            if (User?.Identity?.Name == null)
+                return StatusCode(400);
 
-            //var username = User.Identity.Name;
+            var username = User.Identity.Name;
 
             var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfiguration.IssuerSigningKey));
 
@@ -41,14 +43,16 @@ namespace Vaelastrasz.Server.Controllers
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, model.Username),
-                    new Claim(ClaimTypes.Role, "admin")
+                    new Claim(ClaimTypes.Name, username), 
                 }),
                 Expires = DateTime.Now.AddHours(_jwtConfiguration.ValidLifetime),
                 Issuer = _jwtConfiguration.ValidIssuer,
                 Audience = _jwtConfiguration.ValidAudience,
-                SigningCredentials = new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256Signature)
+                SigningCredentials = new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha512Signature)
             };
+
+            if(_admins.Contains(username))
+                tokenDescriptor.Subject.AddClaim(new Claim(ClaimTypes.Role, "admin"));
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
             return Ok(tokenHandler.WriteToken(token));
