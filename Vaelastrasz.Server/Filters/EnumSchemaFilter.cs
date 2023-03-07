ae03806ffd1e37d557231a7bb17c4a0c2fdaeb6e
Sven Thiel <sven.thiel@uni-jena.de>
2023-03-07 09:49:20 +0100

    remove warnings

--- Vaelastrasz.Server/Filters/EnumSchemaFilter.cs
+++ Vaelastrasz.Server/Filters/EnumSchemaFilter.cs
@@ -14,10 +14,8 @@ namespace Vaelastrasz.Server.Filters
                 model.Enum.Clear();
                 foreach (string enumName in Enum.GetNames(context.Type))
                 {
-                    System.Reflection.MemberInfo memberInfo = context.Type.GetMember(enumName).FirstOrDefault(m => m.DeclaringType == context.Type);
-                    EnumMemberAttribute enumMemberAttribute = memberInfo == null
-                     ? null
-                     : memberInfo.GetCustomAttributes(typeof(EnumMemberAttribute), false).OfType<EnumMemberAttribute>().FirstOrDefault();
+                    System.Reflection.MemberInfo? memberInfo = context.Type.GetMember(enumName).FirstOrDefault(m => m.DeclaringType == context.Type);
+                    EnumMemberAttribute? enumMemberAttribute = memberInfo?.GetCustomAttributes(typeof(EnumMemberAttribute), false).OfType<EnumMemberAttribute>().FirstOrDefault();
                     string label = enumMemberAttribute == null || string.IsNullOrWhiteSpace(enumMemberAttribute.Value)
                      ? enumName
                      : enumMemberAttribute.Value;
