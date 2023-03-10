a2b47ee635530eb47a93ea7a728c630426b91150
Sven Thiel <m6thsv2@googlemail.com>
2023-03-10 13:30:58 +0100

    add functions to modify model

--- Vaelastrasz.Library/Extensions/DataCiteModelExtensions.cs
+++ Vaelastrasz.Library/Extensions/DataCiteModelExtensions.cs
@@ -8,16 +8,46 @@ namespace Vaelastrasz.Library.Extensions
 {
     public static class DataCiteModelExtensions
     {
+        #region CreateDataCiteModel
+
         public static CreateDataCiteModel AddCreator(this CreateDataCiteModel model, string firstname, string lastname)
         {
-            model.Data.Attributes.Creators.Add(new DataCiteCreator(firstname, lastname));
-            return model;
+            try
+            {
+                model.Data.Attributes.Creators.Add(new DataCiteCreator(firstname, lastname));
+                return model;
+            }
+            catch(Exception)
+            {
+                return model;
+            }
         }
 
         public static CreateDataCiteModel AddCreator(this CreateDataCiteModel model, string name, DataCiteCreatorType type)
         {
-            model.Data.Attributes.Creators.Add(new DataCiteCreator(name, type));
-            return model;
+            try
+            {
+                model.Data.Attributes.Creators.Add(new DataCiteCreator(name, type));
+                return model;
+            }
+            catch(Exception)
+            {
+                return model;
+            }
+            
+        }
+
+        public static CreateDataCiteModel RemoveCreator(this CreateDataCiteModel model, int index)
+        {
+            try
+            {
+                model.Data.Attributes.Creators.RemoveAt(index);
+                return model;
+            }
+            catch (Exception)
+            {
+                return model;
+            }
         }
 
         public static CreateDataCiteModel AddTitle(this CreateDataCiteModel model, string title, string language = null, DataCiteTitleType? type = null)
@@ -25,5 +55,48 @@ namespace Vaelastrasz.Library.Extensions
             model.Data.Attributes.Titles.Add(new DataCiteTitle(title, language, type));
             return model;
         }
+
+        public static CreateDataCiteModel RemoveTitle(this CreateDataCiteModel model, int index)
+        {
+            try
+            {
+                model.Data.Attributes.Titles.RemoveAt(index);
+                return model;
+            }
+            catch (Exception)
+            {
+                return model;
+            }
+        }
+
+        public static CreateDataCiteModel SetDoi(this CreateDataCiteModel model, string doi)
+        {
+            try
+            {
+                model.Data.Attributes.Doi = doi;
+                return model;
+            }
+            catch (Exception)
+            {
+                return model;
+            }
+        }
+
+        public static CreateDataCiteModel SetType(this CreateDataCiteModel model, DataCiteType? type)
+        {
+            try
+            {
+                model.Data.Type = type;
+                return model;
+            }
+            catch (Exception)
+            {
+                return model;
+            }
+        }
+
+
+
+        #endregion
     }
 }
