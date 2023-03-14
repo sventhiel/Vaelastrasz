b7ab10e8e2d47d595d0c41b3d52f6835a27b752d
Sven Thiel <m6thsv2@googlemail.com>
2023-03-14 08:50:08 +0100

    add config class for lib services to communicate with server

---
+++ Vaelastrasz.Library/Configurations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vaelastrasz.Library.Configurations
+{
+    public class Configuration
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public Dictionary<string, string> Placeholders { get; set; }
+
+        public Configuration(string username, string password, string host, Dictionary<string, string> placeholders = null)
+        {
+            Username = username;
+            Password = password;
+            Host = host;
+            Placeholders = placeholders ?? new Dictionary<string, string>();
+        }
+
+
+    }
+}
---
+++ Vaelastrasz.Library/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vaelastrasz.Library.Configurations;
+
+namespace Vaelastrasz.Library.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static Configuration AddPlaceholder(this Configuration config, string key, string value)
+        {
+            if (config.Placeholders.ContainsKey(key))
+                return config;
+
+            config.Placeholders.Add(key, value);
+            return config;
+        }
+
+        public static Configuration RemovePlaceholder(this Configuration config, string key)
+        {
+            if (!config.Placeholders.ContainsKey(key))
+                return config;
+
+            config.Placeholders.Remove(key);
+            return config;
+        }
+
+        public static Configuration SetPlaceholders(this Configuration config, Dictionary<string, string> placeholders)
+        {
+            config.Placeholders = placeholders;
+            return config;
+        }
+    }
+}
