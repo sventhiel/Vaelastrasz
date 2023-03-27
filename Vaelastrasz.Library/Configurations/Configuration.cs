09e0093387413d6d197fbaa98b42cd61667c5f4a
Sven Thiel <m6thsv2@googlemail.com>
2023-03-27 09:21:47 +0200

    remove placeholders from config

--- Vaelastrasz.Library/Configurations/Configuration.cs
+++ Vaelastrasz.Library/Configurations/Configuration.cs
@@ -9,16 +9,23 @@ namespace Vaelastrasz.Library.Configurations
         public string Username { get; set; }
         public string Password { get; set; }
         public string Host { get; set; }
-        public Dictionary<string, string> Placeholders { get; set; }
 
-        public Configuration(string username, string password, string host, Dictionary<string, string> placeholders = null)
+        /// <summary>
+        /// The general configuration that is needed to connect to an appropriate proxy.
+        /// </summary>
+        /// <param name="username">The username of the credentials you will use to connect to the proxy.</param>
+        /// <param name="password">The password of the <paramref name="username"/></param>
+        /// <param name="host">The base url of the host proxy system (e.g. https://doi.bexis2.uni-jena.de)</param>
+        public Configuration(string username, string password, string host)
         {
             Username = username;
             Password = password;
             Host = host;
-            Placeholders = placeholders ?? new Dictionary<string, string>();
         }
 
-
+        public string GetBasicAuthorizationHeader()
+        {
+            return $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{Username}:{Password}"))}";
+        }
     }
 }
