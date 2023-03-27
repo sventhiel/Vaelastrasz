0e326e756a8eb944f8505695933151412d8e4e8b
Sven Thiel <m6thsv2@googlemail.com>
2023-03-27 21:06:04 +0200

    remove empty line

--- Vaelastrasz.Library/Services/DOIService.cs
+++ Vaelastrasz.Library/Services/DOIService.cs
@@ -15,6 +15,5 @@ namespace Vaelastrasz.Library.Services
         }
 
 
-
     }
 }
--- Vaelastrasz.Library/Services/DataCiteService.cs
+++ Vaelastrasz.Library/Services/DataCiteService.cs
@@ -1,4 +1,10 @@
-﻿using Vaelastrasz.Library.Configurations;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Vaelastrasz.Library.Configurations;
 using Vaelastrasz.Library.Models;
 
 namespace Vaelastrasz.Library.Services
@@ -6,10 +12,12 @@ namespace Vaelastrasz.Library.Services
     public class DataCiteService
     {
         private readonly Configuration _config;
+        static readonly HttpClient client = new HttpClient();
 
         public DataCiteService(Configuration config) 
         { 
-            _config= config;
+            _config = config;
+            client.DefaultRequestHeaders.Add("Authorization", _config.GetBasicAuthorizationHeader());
         }
 
         public CreateDataCiteModel Create()
@@ -17,13 +25,31 @@ namespace Vaelastrasz.Library.Services
             return new CreateDataCiteModel();
         }
 
-        public void FindByDoi(string doi)
+        public CreateDataCiteModel Create(string s)
         {
+            return new CreateDataCiteModel();
         }
 
-        public void Find()
+        public async Task<ReadDataCiteModel> FindByDoiAsync(string doi)
         {
+            HttpResponseMessage response = await client.GetAsync($"{_config.Host}/api/datacite/{doi}");
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                return null;
+
+            return JsonConvert.DeserializeObject<ReadDataCiteModel>(await response.Content.ReadAsStringAsync());
         }
+
+        //public async Task<List<ReadDataCiteModel>> Find()
+        //{
+        //    HttpResponseMessage response = await client.GetAsync($"{_config.Host}/api/datacite/");
+
+        //    if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        //        return null;
+
+        //    return JsonConvert.DeserializeObject<ReadListDataCiteModel>(await response.Content.ReadAsStringAsync());
+
+        //}
 
         public void Update()
         { }
