912fc0e5e2a4df3fef019dc50f772c342034dad9
Sven Thiel <m6thsv2@googlemail.com>
2023-03-27 21:06:18 +0200

    add tests

---
+++ Vaelastrasz.Library.Tests/Services/DataCiteServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vaelastrasz.Library.Configurations;
+using Vaelastrasz.Library.Services;
+
+namespace Vaelastrasz.Library.Tests.Services
+{
+    public class DataCiteServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+            var config = new Configuration("sventhiel", "sventhiel", "http://localhost:5041");
+
+            var dataCiteService = new DataCiteService(config);
+
+            var x = await dataCiteService.FindByDoiAsync("10.23720%2Fapitest005");
+
+
+        }
+    }
+}
