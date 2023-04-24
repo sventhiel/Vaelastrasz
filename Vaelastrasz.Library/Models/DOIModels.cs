60eca491ced13623aabd65e1014af955466c282e
Sven Thiel <m6thsv2@googlemail.com>
2023-04-24 11:11:20 +0200

    add model for created dois

---
+++ Vaelastrasz.Library/Models/DOIModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vaelastrasz.Library.Models
+{
+    public class CreateDOIModel
+    {
+    }
+
+    public class ReadDOIModel
+    {
+        public string Prefix => DOI.Split('/')[0];
+        public string Suffix => DOI.Split('/')[1];
+        public string DOI { get; set; }
+    }
+}
