6500bb17bd55eb5a790f2c4a42c70f918fca0162
Sven Thiel <m6thsv2@googlemail.com>
2023-07-24 15:47:12 +0200

    add extension to transform different states into server based state

---
+++ Vaelastrasz.Server/Extensions/DataCiteStateTypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Vaelastrasz.Library.Models;
+using Vaelastrasz.Server.Entities;
+
+namespace Vaelastrasz.Server.Extensions
+{
+    public static class DataCiteStateTypeExtensions
+    {
+        public static State ToState(this DataCiteStateType state)
+        {
+            switch(state)
+            {
+                case DataCiteStateType.Draft: return State.Draft;
+                case DataCiteStateType.Findable: return State.Findable;
+                case DataCiteStateType.Registered: return State.Registered;
+
+                default: return State.Draft;
+            }
+        }
+    }
+}
