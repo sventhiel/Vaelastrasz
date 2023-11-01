7ef175668760bc33a8c7db57047fefba672bb265
Sven Thiel <m6thsv2@googlemail.com>
2023-11-01 17:23:01 +0100

    add status to DOI model

--- Vaelastrasz.Library/Models/DOIModels.cs
+++ Vaelastrasz.Library/Models/DOIModels.cs
@@ -1,7 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Runtime.Serialization;
 
 namespace Vaelastrasz.Library.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum DOIStateType
+    {
+        [EnumMember(Value = "findable")]
+        Findable = 1,
+
+        [EnumMember(Value = "registered")]
+        Registered = 2,
+
+        [EnumMember(Value = "draft")]
+        Draft = 3
+    }
+
     public class CreateDOIModel
     {
         public string Prefix { get; set; }
