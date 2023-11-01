4fe659da0ce85be09f31bab61e68a8e6499b032e
Sven Thiel <m6thsv2@googlemail.com>
2023-11-01 17:33:46 +0100

    add state to doi entity

--- Vaelastrasz.Server/Entities/DOI.cs
+++ Vaelastrasz.Server/Entities/DOI.cs
@@ -1,4 +1,5 @@
 ﻿using LiteDB;
+using Vaelastrasz.Library.Models;
 
 namespace Vaelastrasz.Server.Entities
 {
@@ -14,9 +15,15 @@ namespace Vaelastrasz.Server.Entities
         public long Id { get; set; }
         public DateTimeOffset LastUpdateDate { get; set; }
         public string Prefix { get; set; }
+        public DOIStateType State { get; set; }
         public string Suffix { get; set; }
 
         [BsonRef("users")]
         public User User { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Prefix}/{Suffix}";
+        }
     }
 }
