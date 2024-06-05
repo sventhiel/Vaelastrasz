ce0cacfa298f72058155a6e8038fd3d20ef047f8
Sven Thiel <m6thsv2@googlemail.com>
2024-06-05 14:27:29 +0200

    add string extension to replace multiple texts at the same time

---
+++ Vaelastrasz.Library/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Vaelastrasz.Library.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Replace(string text, Dictionary<string, string> replacements)
+        {
+            return Regex.Replace(text, "(" + String.Join("|", replacements.Keys) + ")", delegate (Match m) { return replacements[m.Value]; });
+        }
+    }
+}
