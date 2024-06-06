b4b0fc4d72a7f4aa56c6cd408e672ef16b069e91
Sven Thiel <m6thsv2@googlemail.com>
2024-06-06 15:39:03 +0200

    add function to replace several strings at once

--- Vaelastrasz.Library/Extensions/StringExtensions.cs
+++ Vaelastrasz.Library/Extensions/StringExtensions.cs
@@ -7,7 +7,7 @@ namespace Vaelastrasz.Library.Extensions
 {
     public static class StringExtensions
     {
-        public static string Replace(string text, Dictionary<string, string> replacements)
+        public static string Replace(this string text, Dictionary<string, string> replacements)
         {
             return Regex.Replace(text, "(" + String.Join("|", replacements.Keys) + ")", delegate (Match m) { return replacements[m.Value]; });
         }
