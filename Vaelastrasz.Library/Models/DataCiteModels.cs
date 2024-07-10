a1d86035c250d357ef4cefc198a41cc84a49e65e
Sven Thiel <m6thsv2@googlemail.com>
2024-07-10 17:06:03 +0200

    update datacite models

--- Vaelastrasz.Library/Models/DataCiteModels.cs
+++ Vaelastrasz.Library/Models/DataCiteModels.cs
@@ -171,6 +171,8 @@ namespace Vaelastrasz.Library.Models
     {
         public ReadDataCiteAttributesModel()
         {
+            GeoLocations = new List<DataCiteGeoLocation>();
+            AlternateIdentifiers = new List<DataCiteAlternateIdentifier>();
             Creators = new List<DataCiteCreator>();
             Contributors = new List<DataCiteContributor>();
             Dates = new List<DataCiteDate>();
@@ -185,63 +187,125 @@ namespace Vaelastrasz.Library.Models
             FundingReferences = new List<DataCiteFundingReference>();
         }
 
-        [JsonProperty("contributors")]
-        public List<DataCiteContributor> Contributors { get; set; }
-
-        [JsonProperty("creators")]
-        public List<DataCiteCreator> Creators { get; set; }
-
-        [JsonProperty("dates")]
-        public List<DataCiteDate> Dates { get; set; }
-
-        [JsonProperty("descriptions")]
-        public List<DataCiteDescription> Descriptions { get; set; }
-
-        [JsonProperty("doi")]
-        public string Doi { get; set; }
-
-        [JsonProperty("formats")]
-        public List<string> Formats { get; set; }
-
-        [JsonProperty("fundingReferences")]
-        public List<DataCiteFundingReference> FundingReferences { get; set; }
-
-        [JsonProperty("identifiers")]
-        public List<DataCiteIdentifier> Identifiers { get; set; }
-
-        [JsonProperty("language")]
-        public string Language { get; set; }
-
-        [JsonProperty("publicationYear")]
-        public int PublicationYear { get; set; }
-
-        [JsonProperty("publisher")]
-        public string Publisher { get; set; }
-
-        [JsonProperty("relatedIdentifiers")]
-        public List<DataCiteRelatedIdentifier> RelatedIdentifiers { get; set; }
-
-        [JsonProperty("rightsList")]
-        public List<DataCiteRight> RightsList { get; set; }
-
-        [JsonProperty("sizes")]
-        public List<string> Sizes { get; set; }
-
         [JsonProperty("state")]
         public DataCiteStateType State { get; set; }
 
+        //
+        [JsonProperty("viewCount")]
+        public long ViewCount { get; set; }
+
+        [JsonProperty("downloadCount")]
+        public long DownloadCount { get; set; }
+
+        [JsonProperty("referenceCount")]
+        public long ReferenceCount { get; set; }
+
+        [JsonProperty("created")]
+        public string Created { get; set; }
+
+        [JsonProperty("registered")]
+        public string Registered { get; set; }
+
+        [JsonProperty("published")]
+        public string Published { get; set; }
+
+        [JsonProperty("updated")]
+        public string Updated { get; set; }
+
+        // 12 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/alternateidentifier/
+        [JsonProperty("alternateIdentifiers")]
+        public List<DataCiteAlternateIdentifier> AlternateIdentifiers { get; set; }
+
+        // 7 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/contributor/
+        [JsonProperty("contributors")]
+        public List<DataCiteContributor> Contributors { get; set; }
+
+        // 2 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/creator/
+        [Cardinality(Minimum = 1)]
+        [JsonProperty("creators")]
+        public List<DataCiteCreator> Creators { get; set; }
+
+        // 8 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/date/
+        [JsonProperty("dates")]
+        public List<DataCiteDate> Dates { get; set; }
+
+        // 17 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/description/
+        [JsonProperty("descriptions")]
+        public List<DataCiteDescription> Descriptions { get; set; }
+
+        // 0
+        [Required]
+        [JsonProperty("doi")]
+        public string Doi { get; set; }
+
+        [JsonProperty("prefix")]
+        public string Prefix { get; set; }
+
+        [JsonProperty("suffix")]
+        public string Suffix { get; set; }
+
+        // 14 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/format/
+        [JsonProperty("formats")]
+        public List<string> Formats { get; set; }
+
+        // 19 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/fundingreference/
+        [JsonProperty("fundingReferences")]
+        public List<DataCiteFundingReference> FundingReferences { get; set; }
+
+        // 18 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/geolocation/
+        [JsonProperty("geoLocations")]
+        public List<DataCiteGeoLocation> GeoLocations { get; set; }
+
+        // 1 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/identifier/
+        [JsonProperty("identifiers")]
+        public List<DataCiteIdentifier> Identifiers { get; set; }
+
+        // 9 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/language/
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        // 5 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/publicationyear/
+        [Required]
+        [JsonProperty("publicationYear")]
+        public int PublicationYear { get; set; }
+
+        // 4 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/publisher/
+        [Required]
+        [JsonProperty("publisher")]
+        public DataCitePublisher Publisher { get; set; }
+
+        // 12 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/relatedidentifier/
+        [JsonProperty("relatedIdentifiers")]
+        public List<DataCiteRelatedIdentifier> RelatedIdentifiers { get; set; }
+
+        // 16 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/rights/#
+        [JsonProperty("rightsList")]
+        public List<DataCiteRight> RightsList { get; set; }
+
+        // 13 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/size/
+        [JsonProperty("sizes")]
+        public List<string> Sizes { get; set; }
+
+        // 6 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/subject/
         [JsonProperty("subjects")]
         public List<DataCiteSubject> Subjects { get; set; }
 
+        // 3 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/title/
+        [Cardinality(Minimum = 1)]
         [JsonProperty("titles")]
         public List<DataCiteTitle> Titles { get; set; }
 
+        // 10 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/resourcetype/
+        [Required]
         [JsonProperty("types")]
         public DataCiteTypes Types { get; set; }
 
+        // 0
+        [Required]
         [JsonProperty("url")]
         public string URL { get; set; }
 
+        // 15 - https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/version/
         [JsonProperty("version")]
         public string Version { get; set; }
     }
