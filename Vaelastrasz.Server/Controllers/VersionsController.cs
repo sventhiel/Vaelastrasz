7bbcc42c7be7dcc89466bb3d4c871170e14e6cb6
sventhiel <m6thsv2@googlemail.com>
2025-04-30 13:21:28 +0200

    add api call to return version of server and lib

---
+++ Vaelastrasz.Server/Controllers/VersionsController.cs
@@ -0,0 +1,39 @@
+﻿using LiteDB;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Vaelastrasz.Server.Controllers
+{
+    [ApiController, Route("api")]
+    public class VersionsController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+        private ConnectionString _connectionString;
+
+        public VersionsController(ILogger<UsersController> logger, IConfiguration configuration, ConnectionString connectionString)
+        {
+            _connectionString = connectionString;
+            _logger = logger;
+        }
+
+        [HttpGet("versions")]
+        public IActionResult GetVersions()
+        {
+            // Beispiel für Newtonsoft.Json (ersetze dies je nach deinem Paket)
+            var assembly = AppDomain.CurrentDomain.GetAssemblies().Single(a => a.GetName().Name.Equals($"Vaelastrasz.Library", StringComparison.OrdinalIgnoreCase));
+
+            if (assembly == null)
+                return NotFound("The package 'Vaelastrasz.Library' d");
+
+            var version_library = assembly.GetName().Version?.ToString();
+
+            var version_application = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+            return Ok(new
+            {
+                Library = version_library,
+                Application = version_application
+            });
+        }
+    }
+}
